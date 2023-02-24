434f61307d9c4abd2f1bf9740973a0de2498daf3
TimoteoGambella <timi.gambella@hotmail.com>
2023-02-24 16:24:32 -0300

    project29

--- src/views/Project29.jsx
+++ src/views/Project29.jsx
@@ -28,17 +28,12 @@ export default function Project29({setDirUrl}){
         heart.classList.add('fas')
         heart.classList.add('fa-heart')
 
-        const x = e.clientX
-        const y = e.clientY
 
-        const leftOffset = e.target.offsetLeft
-        const topOffset = e.target.offsetTop
+        let x = e.clientX - e.target.offsetLeft
+        let y = e.clientY - e.target.offsetTop
 
-        const xInside = x - leftOffset
-        const yInside = y - topOffset
-
-        heart.style.top = `${yInside}px`
-        heart.style.left = `${xInside}px`
+        heart.style.left = `${x}px`
+        heart.style.top = `${y}px`
 
         loveMe.appendChild(heart)
 
