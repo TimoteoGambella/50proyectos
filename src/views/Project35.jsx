665e8dc0c9cb6bac83b3984feefb3261193d60bd
TimoteoGambella <91702810+TimoteoGambella@users.noreply.github.com>
2023-02-28 13:22:00 -0300

    project35

--- src/views/Project35.jsx
+++ src/views/Project35.jsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react"
 import "../styles/project35/project35.scss"
 
 export default function Project35({setDirUrl}){
+    
+    const [imgNow,setImgNow]=useState(0)
+    const [arrayImgs,setArrayImgs]=useState([])
 
     useEffect(() => {
 
@@ -13,64 +16,47 @@ export default function Project35({setDirUrl}){
         if (html.classList.contains('dark')) {
             html.classList.remove('dark')
         }
-
+        
     }, []);// eslint-disable-line react-hooks/exhaustive-deps
-
-    const imgs = document.getElementById('imgs')
-    const leftBtn = document.getElementById('left')
-    const rightBtn = document.getElementById('right')
     
-    const img = document.querySelectorAll('#imgs img')
-    
-    let idx = 0
-    
-    let interval = setInterval(run, 2000)
-    
-    function run() {
-        idx++
-        changeImage()
-    }
-    
-    function changeImage() {
-        if(idx > img.length - 1) {
-            idx = 0
-        } else if(idx < 0) {
-            idx = img.length - 1
+    useEffect(() => {
+        if(arrayImgs.length===0){
+            setArrayImgs([
+                "https://images.unsplash.com/photo-1599394022918-6c2776530abb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1458&q=80",
+                "https://images.unsplash.com/photo-1593642632559-0c6d3fc62b89?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+                "https://images.unsplash.com/photo-1599423300746-b62533397364?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+                "https://images.unsplash.com/photo-1599561046251-bfb9465b4c44?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1492&q=80"
+            ])
+        }else{
+            changeImage(imgNow)
         }
+    }, [arrayImgs])
     
-        imgs.style.transform = `translateX(${-idx * 500}px)`
+    function changeImage(idx) {
+        document.getElementById(idx).style.transform = `translateX(${-idx * 500}px)`
+        
+        setTimeout(async() => {
+            console.log(idx)
+            if(idx===arrayImgs.length-1){
+                setImgNow(0)
+                changeImage(0)
+                setArrayImgs([])
+            }else{
+                setImgNow(idx)
+                changeImage(idx+1)
+            }
+        }, 1500);
     }
-    
-    function resetInterval() {
-        clearInterval(interval)
-        interval = setInterval(run, 2000)
-    }
-    
-    rightBtn.addEventListener('click', () => {
-        idx++
-        changeImage()
-        resetInterval()
-    })
-    
-    leftBtn.addEventListener('click', () => {
-        idx--
-        changeImage()
-        resetInterval()
-    })
     
     return(
         <div className="project35-container">
             <div className="carousel">
                 <div className="image-container" id="imgs">
-                    <img src="https://images.unsplash.com/photo-1599394022918-6c2776530abb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1458&q=80" alt="first-image"/>
-                    <img src="https://images.unsplash.com/photo-1593642632559-0c6d3fc62b89?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80" alt="second-image"/>
-                    <img src="https://images.unsplash.com/photo-1599423300746-b62533397364?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80" alt="third-image"/>
-                    <img src="https://images.unsplash.com/photo-1599561046251-bfb9465b4c44?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1492&q=80" alt="fourth-image"/>
-                </div>
-
-                <div className="buttons-container">
-                    <button id="left" className="btn">Prev</button>
-                    <button id="right" className="btn">Next</button>
+                    {arrayImgs.map((img,i)=>{
+                        return(
+                            <img src={img} alt={`img${i}`} key={i} id={i}/>
+                        )
+                    })}
                 </div>
             </div>
         </div>
